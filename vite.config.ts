cab2aaf8f333f788e4aae143607d0e28ecd505e3
Kononenko Daniil <neokv210@gmail.com>
2024-10-03 22:31:13 +0300

    Update in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,49 +1,38 @@
 // vite.config.ts
 import { defineConfig } from 'vite';
 
-import typescript from '@rollup/plugin-typescript';
-import path from 'path';
-import { typescriptPaths } from 'rollup-plugin-typescript-paths';
+import path, { dirname } from 'path';
 import react from '@vitejs/plugin-react';
 import dts from 'vite-plugin-dts';
+import { fileURLToPath } from 'url';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   plugins: [
     react(),
-    dts({
-      insertTypesEntry: true,
-    }),
+    dts({ outDir: 'dist/dts', tsconfigPath: './tsconfig.json' }),
+    tsconfigPaths({ projects: ['./tsconfig.json'] }),
   ],
   resolve: {
     alias: [
       {
         find: '~',
-        replacement: path.resolve(__dirname, './src'),
+        replacement: path.resolve(dirname(fileURLToPath(import.meta.url)), './src'),
       },
     ],
   },
   build: {
-    manifest: true,
-    minify: true,
-    reportCompressedSize: true,
+    outDir: 'dist',
+    emptyOutDir: true,
+    sourcemap: true,
     lib: {
-      entry: path.resolve(__dirname, 'src/main.ts'),
-      fileName: 'main',
-      name: '@greenton/sdk-react',
+      name: 'greenton.sdkReact',
+      entry: 'src/main.ts',
       formats: ['es', 'cjs'],
+      fileName: 'main',
     },
     rollupOptions: {
       external: ['react', 'react-dom', '@tonconnect/ui-react'],
-      plugins: [
-        typescriptPaths({
-          preserveExtensions: true,
-        }),
-        typescript({
-          sourceMap: false,
-          declaration: true,
-          outDir: 'dist',
-        }),
-      ],
       output: {
         globals: {
           react: 'React',
